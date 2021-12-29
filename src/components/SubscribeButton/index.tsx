91c0975ac4bd6923e7a13a62e4eb143754094ece
Filipe Lima <75786496+filipemsl@users.noreply.github.com>
2021-12-29 09:52:56 -0300

    aplicação finalizada

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,42 @@
+import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const session = useSession();
+  const router = useRouter();
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+    if (session.data.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe?.redirectToCheckout({ sessionId: sessionId });
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={handleSubscribe}
+      className={styles.subscribeButton}
+    >
+      Subscribe now
+    </button>
+  );
+}
